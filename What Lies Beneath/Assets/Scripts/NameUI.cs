cce39f27e6b268f934d75f22a2d8c736f9fba4f9
DAV010902 <83784551+DAV010902@users.noreply.github.com>
2023-09-13 17:52:01 -0600

    Added Name to UI Stat Panel

---
+++ What Lies Beneath/Assets/Scripts/NameUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NameUI : MonoBehaviour
+{
+    public TMP_Text displayText;
+    public HeroStateMachine heroStateMachine;
+    private string characterName = "???";
+
+    // Start is called before the first frame update
+    void Start() {
+        characterName = heroStateMachine.character.nombre;
+        SetCharacterName(characterName);
+    }
+
+    public void SetCharacterName(string charName) {
+        displayText.text = charName;
+    }
+}
